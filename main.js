d6828af44fa100264ebb2ca1b7cc050378a006e2
Hayden Wagner <haydenblainewagner@gmail.com>
2016-05-22 22:27:31 -0400

    Added functions to handle the client side response if a file already exists on the server...(ask user if they want to edit the existing file or not). Also made function that clears this message and input box if the user selects no to this prompt

--- main.js
+++ main.js
@@ -1,11 +1,69 @@
 function makeNewEntry(){
-    var fileName = "/" + document.getElementById('newFileInput').value + '.json';
-    console.log(fileName);
-    if(fileName){
+    var value = document.getElementById('newFileInput').value;
+
+    if ( value === undefined || value === '' ){
+        return;
+    }
+    else{
+        var fileName = "/" + value + '.json';
+        console.log(fileName);
+
         //SEND AS JSON FILE TO 'node-practice.js'
 
         $.get(fileName, function(res){
-            console.log(res);
+            if(res === 'File already exists'){
+                fileExists(res);
+            }
+            else{
+                console.log(res);
+            }
         });
     }
+
+
+}
+
+
+function newButton(text, func){
+    var button = document.createElement('button');
+    button.innerHTML = text;
+    button.onclick = func;
+
+    return button;
+}
+
+function fileExists(res){
+    var div = document.createElement('div');
+    var p = document.createElement('p');
+    var input = document.getElementById('newFileInput');
+
+    div.id = 'fileExistsDiv';
+    p.innerHTML = res + ', would you like to edit the existing file?';
+
+    div.appendChild(p);
+    div.appendChild( newButton('Yes', fileExistsYes) );
+    div.appendChild( newButton('No', fileExistsNo) );
+
+    document.body.appendChild(div);
+
+    input.style.background = 'red';
+}
+
+function fileExistsYes(){
+    console.log(this);
+    console.log('yes, edit the existing file');
+}
+
+function fileExistsNo(){
+    var input = document.getElementById('newFileInput');
+    var fileExistsDiv = document.getElementById('fileExistsDiv');
+
+    input.value = '';
+    input.style.background = 'none';
+    input.focus();
+
+    document.body.removeChild(fileExistsDiv);
+
+    console.log(this);
+    console.log("no, don't edit the existing file");
 }
